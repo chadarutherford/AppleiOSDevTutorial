55af721ec710170f848d28e407aa8e1316344445
chadarutherford <chad_rutherford@outlook.com>
2023-01-06 13:43:04 -0500

    Adds Scrumdinger Project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Chad Rutherford on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
