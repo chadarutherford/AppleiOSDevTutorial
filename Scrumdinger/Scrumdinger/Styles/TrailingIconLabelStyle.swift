195bd0da260ebebae553205beab17d6b8d95296b
chadarutherford <chad_rutherford@outlook.com>
2023-01-06 14:24:11 -0500

    Adds accessibility to CardView

---
+++ Scrumdinger/Scrumdinger/Styles/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Chad Rutherford on 1/6/23.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
