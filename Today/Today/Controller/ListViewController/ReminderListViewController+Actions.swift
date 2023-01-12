35a941acc5b09858933d8ef45c7ca24e47d370a3
chadarutherford <chad_rutherford@outlook.com>
2023-01-12 14:14:58 -0500

    Creates custom button action

---
+++ Today/Today/Controller/ListViewController/ReminderListViewController+Actions.swift
@@ -0,0 +1,15 @@
+//
+//  ReminderListViewController+Actions.swift
+//  Today
+//
+//  Created by Chad Rutherford on 1/12/23.
+//
+
+import UIKit
+
+extenison ReminderListViewController {
+    @objc func didPressDoneButton(_ sender: ReminderDoneButton) {
+        guard let id = sender.id else { return }
+        completeReminder(with: id)
+    }
+}
---
+++ Today/Today/View/Custom Views/RemidnerDoneButton.swift
@@ -0,0 +1,13 @@
+//
+//  RemidnerDoneButton.swift
+//  Today
+//
+//  Created by Chad Rutherford on 1/12/23.
+//
+
+import UIKit
+
+final class ReminderDoneButton: UIButton {
+    var id: Reminder.ID
+    
+}
