3c4352b88cdf0fcd3e2b796ddd7f7f56ff04fc27
chadarutherford <chad_rutherford@outlook.com>
2023-01-21 16:48:42 -0500

    Creates reminder view and sets up enumeration for rows

---
+++ Today/Today/Controller/ReminderViewController/ReminderViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ReminderViewController.swift
+//  Today
+//
+//  Created by Chad Rutherford on 1/21/23.
+//
+
+import UIKit
+
+final class ReminderViewController: UICollectionViewController {
+    var reminder: Reminder
+    
+    init(reminder: Reminder) {
+        self.reminder = reminder
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        listConfiguration.showsSeparators = false
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+        super.init(collectionViewLayout: listLayout)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("Always initialize ReminderViewController using init(reminder:)")
+    }
+    
+    func text(for row: Row) -> String? {
+        switch row {
+        case .viewDate: return reminder.dueDate.dayText
+        case .viewNotes: return reminder.notes
+        case .viewTime: return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        case .viewTitle: return reminder.title
+        }
+    }
+}
