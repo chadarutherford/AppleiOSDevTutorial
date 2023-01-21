4a0ae06f7e87f1ae669a58b9cb3ed28481035cf9
chadarutherford <chad_rutherford@outlook.com>
2023-01-21 16:53:53 -0500

    Sets up data source

--- Today/Today/Controller/ReminderViewController/ReminderViewController.swift
+++ Today/Today/Controller/ReminderViewController/ReminderViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 final class ReminderViewController: UICollectionViewController {
+    private typealias DataSource = UICollectionViewDiffableDataSource<Section, Row>
+    
     var reminder: Reminder
+    private var dataSource: DataSource!
     
     init(reminder: Reminder) {
         self.reminder = reminder
@@ -21,6 +24,23 @@ final class ReminderViewController: UICollectionViewController {
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("Always initialize ReminderViewController using init(reminder:)")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+    }
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = text(for: row)
+        contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        contentConfiguration.image = row.image
+        cell.contentConfiguration = contentConfiguration
+        cell.tintColor = .todayPrimaryTint
     }
     
     func text(for row: Row) -> String? {
