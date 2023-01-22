16ea1f4e4737b053c3e74dee64d4efe53653b01a
chadarutherford <chad_rutherford@outlook.com>
2023-01-22 18:38:11 -0500

    Deleting reminders

--- Today/Today/Controller/ListViewController/ReminderListViewController.swift
+++ Today/Today/Controller/ListViewController/ReminderListViewController.swift
@@ -54,8 +54,21 @@ class ReminderListViewController: UICollectionViewController {
     private func listLayout() -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
         listConfiguration.showsSeparators = false
+        listConfiguration.trailingSwipeActionsConfigurationProvider = makeSwipeActions
         listConfiguration.backgroundColor = .clear
         return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+    
+    private func makeSwipeActions(for indexPath: IndexPath?) -> UISwipeActionsConfiguration? {
+        guard let indexPath = indexPath, let id = dataSource.itemIdentifier(for: indexPath) else { return nil }
+        let deleteActionTitle = NSLocalizedString("Delete", comment: "Delete Action Title")
+        let deleteAction = UIContextualAction(style: .destructive, title: deleteActionTitle) { [weak self] _, _, completion in
+            self?.deleteReminder(with: id)
+            self?.updateSnapshot()
+            completion(false)
+            
+        }
+        return UISwipeActionsConfiguration(actions: [deleteAction])
     }
     
     static func makeReminderListViewController() -> ReminderListViewController {
