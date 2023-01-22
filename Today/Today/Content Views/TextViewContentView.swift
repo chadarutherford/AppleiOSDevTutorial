c093f1cbd3b4ae5d53270724b78c6efc3d7a65a6
chadarutherford <chad_rutherford@outlook.com>
2023-01-22 18:17:59 -0500

    Editing reminders

--- Today/Today/Content Views/TextViewContentView.swift
+++ Today/Today/Content Views/TextViewContentView.swift
@@ -11,6 +11,7 @@ import UIKit
 final class TextViewContentView: UIView, UIContentView {
     struct Configuration: UIContentConfiguration {
         var text: String? = ""
+        var onChange: (String) -> Void = { _ in }
         
         func makeContentView() -> UIView & UIContentView {
             TextViewContentView(self)
@@ -33,6 +34,7 @@ final class TextViewContentView: UIView, UIContentView {
         super.init(frame: .zero)
         addPinnedSubview(textView, height: 200)
         textView.backgroundColor = nil
+        textView.delegate = self
         textView.font = UIFont.preferredFont(forTextStyle: .body)
     }
     
@@ -50,5 +52,12 @@ final class TextViewContentView: UIView, UIContentView {
 extension UICollectionViewListCell {
     func textViewConfiguration() -> TextViewContentView.Configuration {
         TextViewContentView.Configuration()
+    }
+}
+
+extension TextViewContentView: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        guard let configuration = configuration as? TextViewContentView.Configuration else { return }
+        configuration.onChange(textView.text)
     }
 }
