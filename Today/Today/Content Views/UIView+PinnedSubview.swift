8199fd92cfc19206b348f3d46e30f2776b389c5d
chadarutherford <chad_rutherford@outlook.com>
2023-01-21 20:10:03 -0500

    Modifies constraints

--- Today/Today/Content Views/UIView+PinnedSubview.swift
+++ Today/Today/Content Views/UIView+PinnedSubview.swift
@@ -11,13 +11,15 @@ extension UIView {
     func addPinnedSubview(_ subview: UIView, height: CGFloat? = nil, insets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)) {
         addSubview(subview)
         subview.translatesAutoresizingMaskIntoConstraints = false
-        subview.topAnchor.constraint(equalTo: topAnchor, constant: insets.top).isActive = true
-        subview.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets.left).isActive = true
-        trailingAnchor.constraint(equalTo: subview.trailingAnchor, constant: insets.right).isActive = true
-        bottomAnchor.constraint(equalTo: subview.bottomAnchor, constant: insets.bottom).isActive = true
+        NSLayoutConstraint.activate([
+            subview.topAnchor.constraint(equalTo: topAnchor, constant: insets.top),
+            subview.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets.left),
+            trailingAnchor.constraint(equalTo: subview.trailingAnchor, constant: insets.right),
+            bottomAnchor.constraint(equalTo: subview.bottomAnchor, constant: insets.bottom)
+        ])
         
         if let height = height {
-            subview.heightAnchor.constraint(equalToConstant: height).isActive = true
+            NSLayoutConstraint.activate([subview.heightAnchor.constraint(equalToConstant: height)])
         }
     }
 }
