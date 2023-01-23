b051a5c0fa68c321577d44e5b2f395f56224e319
chadarutherford <chad_rutherford@outlook.com>
2023-01-22 22:33:59 -0500

    Adds Earthquakes project

---
+++ Earthquakes/Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Chad Rutherford on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
